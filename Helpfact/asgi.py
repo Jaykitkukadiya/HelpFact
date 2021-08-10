a6144b057b6706038e6dd217a13a5a419c629c7d
Jaykitkukadiya <jaykitkukadiya0@gmail.com>
2021-08-10 20:48:12 +0530

    df

--- Helpfact/asgi.py
+++ Helpfact/asgi.py
@@ -11,15 +11,15 @@ import os
 import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Helpfact.settings')
 # django.setup()
-
-from channels.routing import ProtocolTypeRouter , URLRouter
 from django.core.asgi import get_asgi_application
+asgiapp = get_asgi_application()
+from channels.routing import ProtocolTypeRouter , URLRouter
 from channels.auth import AuthMiddlewareStack
 from main.routing import websocket_urlpatterns
 from channels.security.websocket import AllowedHostsOriginValidator
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": asgiapp,
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
