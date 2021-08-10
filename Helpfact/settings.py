f287362bb8247f0f32c3a6fbe603567fc66f508d
Jaykitkukadiya <jaykitkukadiya0@gmail.com>
2021-08-10 23:05:38 +0530

    ..

--- Helpfact/settings.py
+++ Helpfact/settings.py
@@ -90,7 +90,7 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [("127.0.0.1", 6379)],
-            # "symmetric_encryption_keys": [SECRET_KEY]
+            "symmetric_encryption_keys": [SECRET_KEY]
         },
     },
 }
