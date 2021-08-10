48cf6fd6dbf80e94bfb910cca2fc0e007d586904
Jaykitkukadiya <jaykitkukadiya0@gmail.com>
2021-08-10 18:39:18 +0530

    d11

--- Helpfact/asgi.py
+++ Helpfact/asgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'friendship.settings')
+django.setup()
 
 from channels.routing import ProtocolTypeRouter , URLRouter
 from django.core.asgi import get_asgi_application
